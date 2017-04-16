29d0ed943537577adf432763d1559da66b19cb84
Alacritas <jhwheeler@gmail.com>
2017-04-16 22:18:56 +0300

    Change slide down to fade in for challenge

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Challenge extends React.Component {
   render() {
     return(
-      <div className="challenge slideDown">
+      <div className="challenge fade-in">
         <div className="row">
           <div className="challenge-content">
             <h3>Challenge:</h3>
