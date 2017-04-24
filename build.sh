94cd69795dd8a43fd016ceb015cb7c3b55c5bbc9
Alacritas <jhwheeler@gmail.com>
2017-04-24 20:01:53 +0300

    Tweak docker build script

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ function npm_run_build {
 }
 
 function build_simple_mongo {
-  docker build -f ./data/Dockerfile -t simple-mongo:0.1 .
+  docker build -f ./data/Dockerfile -t simple-mongo .
 }
 
 function build_simple_spirit {
-  docker build -f ./Dockerfile -t simple-spirit:0.1 .
+  docker build -f ./Dockerfile -t simple-spirit .
 }
 
 function import_db {
-  docker exec -it simplespirit_mongo_1 mongoimport --db simple-spirit --collection users --file users.json
-  docker exec -it simplespirit_mongo_1 mongoimport --db simple-spirit --collection maxims --file maxims.json
-  docker exec -it simplespirit_mongo_1 mongoimport --db simple-spirit --collection sessions --file sessions.json
+  docker exec -it simple-mongo mongoimport --db simple-spirit --collection users --file users.json
+  docker exec -it simple-mongo mongoimport --db simple-spirit --collection koans --file koans.json
+  docker exec -it simple-mongo mongoimport --db simple-spirit --collection sessions --file sessions.json
 }
 
 function setup {
