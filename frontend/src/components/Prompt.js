66189b834b11489d178e62cccc8fefba25468e28
Alacritas <jhwheeler@gmail.com>
2017-09-11 09:26:36 +0300

    Reformat prompt button

--- frontend/src/components/Prompt.js
+++ frontend/src/components/Prompt.js
@@ -5,11 +5,8 @@ export default class Prompt extends React.Component {
   render() {
     return(
       <div className="prompt row">
-        <button className="prompt-button" onClick={() => this.props.onClick()}>
-          <div className="prompt-button-text">
-            {this.props.prompt}
-          </div>
-          <img className="arrow" src={arrow}/>
+        <button className="button prompt-button" onClick={() => this.props.onClick()}>
+          <p className="prompt-button-text">{this.props.prompt}</p>
         </button>
       </div>
     );
